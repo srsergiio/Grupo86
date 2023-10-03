9e18dd3bfd219fdb517b7016adcaf3f334779d5f
srsergiio <sesquivelsaibene@gmail.com>
2023-10-03 10:37:24 -0300

    .
    .

--- ProyectoFinal/src/Entidades/Paquete.java
+++ ProyectoFinal/src/Entidades/Paquete.java
@@ -3,6 +3,62 @@ import Entidades.Ciudad;
 
 public class Paquete {
 
+    public Paquete(int idPaquete, Ciudad origen, Ciudad destino, Alojamiento alojamiento, Pasaje pasaje) {
+        this.idPaquete = idPaquete;
+        this.origen = origen;
+        this.destino = destino;
+        this.alojamiento = alojamiento;
+        this.pasaje = pasaje;
+    }
+
+    public Paquete() {
+    }
+
+    public int getIdPaquete() {
+        return idPaquete;
+    }
+
+    public void setIdPaquete(int idPaquete) {
+        this.idPaquete = idPaquete;
+    }
+
+    public Ciudad getOrigen() {
+        return origen;
+    }
+
+    public void setOrigen(Ciudad origen) {
+        this.origen = origen;
+    }
+
+    public Ciudad getDestino() {
+        return destino;
+    }
+
+    public void setDestino(Ciudad destino) {
+        this.destino = destino;
+    }
+
+    public Alojamiento getAlojamiento() {
+        return alojamiento;
+    }
+
+    public void setAlojamiento(Alojamiento alojamiento) {
+        this.alojamiento = alojamiento;
+    }
+
+    public Pasaje getPasaje() {
+        return pasaje;
+    }
+
+    public void setPasaje(Pasaje pasaje) {
+        this.pasaje = pasaje;
+    }
+
+    @Override
+    public String toString() {
+        return "Paquete{" + "idPaquete=" + idPaquete + ", origen=" + origen + ", destino=" + destino + ", alojamiento=" + alojamiento + ", pasaje=" + pasaje + '}';
+    }
+
     private int idPaquete;
 
     private Ciudad origen;
@@ -12,4 +68,6 @@ public class Paquete {
     private Alojamiento alojamiento;
 
     private Pasaje pasaje;
+    
+    
 }
