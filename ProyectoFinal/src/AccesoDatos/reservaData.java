aac5902c7dd4bd71d6ecabbcaa352456c038290b
paulinaroldan <roldanpaulina04@gmail.com>
2023-10-24 21:02:23 -0300

    reservaData

--- ProyectoFinal/src/AccesoDatos/reservaData.java
+++ ProyectoFinal/src/AccesoDatos/reservaData.java
@@ -5,10 +5,49 @@
  */
 package AccesoDatos;
 
+import Entidades.Reserva;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Administrador
  */
 public class reservaData {
-    
+    private List<Reserva> reservas;
+    private static Connection con;
+   
+    public reservaData(){
+        reservas = new ArrayList<>(); 
+         if (con == null) {
+            con = Conexion.getConexion(); 
+        }
+    }
+  
+     public static List<Reserva> listarReserva(){
+          List <Reserva> reservas =new ArrayList<>();
+          try{
+             String sql = "SELECT * FROM reserva";
+             PreparedStatement ps = con.prepareStatement(sql);
+             ResultSet rs= ps.executeQuery();
+          while (rs.next()){
+                Reserva reserva= new Reserva();
+                reserva.setIdReserva(rs.getInt("idReserva"));
+                reserva.setIdCliente(rs.getInt("idCliente"));
+                reserva.setIdPaquete(rs.getInt("idPaquete"));
+                reserva.setFechaReserva(rs.getDate("fechaReserva"));
+                reserva.setFechaViaje(rs.getInt("fechaViaje"));
+                reservas.add(reserva);
+            }
+                ps.close();
+            }catch(SQLException ex){
+                    JOptionPane.showMessageDialog(null, "Error al acceder a la tabla reserva"+ex.getMessage());
+                    }
+        return reservas;
+    }
 }
